2148700b8829c4f5557d8dcc4f82ea8ad90a5581
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2025-04-19 16:11:22 -0400

    first commit

---
+++ AbilityIdLookupPlugin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using AbilityIdLookupPlugin.Windows;
+using Frosty.Controls;
+using Frosty.Core;
+using Frosty.Core.Controls;
+using Frosty.Core.Windows;
+using FrostySdk;
+using FrostySdk.IO;
+using FrostySdk.Managers;
+using FrostySdk.Resources;
+
+namespace AbilityIdLookupPlugin
+{
+    public class AbilityIdGenerateCacheMenuExtension : MenuExtension
+    {
+        public override string TopLevelMenuName => "Tools";
+        public override string SubLevelMenuName => "Generate Cache";
+        public override string MenuItemName => "Ability Id Cache";
+        public override ImageSource Icon => new ImageSourceConverter().ConvertFromString("pack://application:,,,/FrostyEditor;component/Images/Database.png") as ImageSource;
+
+        public static string CacheDirectory = System.AppDomain.CurrentDomain.BaseDirectory + @"Plugins\Caches\";
+        public static string CacheFilePath = CacheDirectory + Enum.GetName(typeof(ProfileVersion), ProfilesLibrary.DataVersion) + "_AbilityIdPlugin_Cache.cache";
+        public static int version = 0x00000001;
+
+        public override RelayCommand MenuItemClicked => new RelayCommand((o) =>
+        {
+            FrostyTaskWindow.Show("Creating Cache", "Enumerating EBX Files...", (task) =>
+            {
+                CreateCache(task);
+            });
+        });
+
+        private void CreateCache(FrostyTaskWindow task)
+        {
+            AssetManager AM = App.AssetManager;
+            List<EbxAssetEntry> ebxFiles = AM.EnumerateEbx(type: "PlayerAbilityAsset").ToList();
+            int index = 0;
+            int count = ebxFiles.Count;
+            List<UInt32> ids = new List<UInt32>();
+            List<string> paths = new List<string>();
+            ebxFiles.ForEach((abilityEntry) =>
+            {
+                if (abilityEntry.IsAdded) {
+                    App.Logger.Log("Skipping " + abilityEntry.Name + " as it is an added asset.");
+                    return;
+                }
+
+                EbxAsset abilityAsset = AM.GetEbx(abilityEntry);
+
+                task.Update($"Processing ({index + 1}/{count}): {abilityEntry.Filename}", index / count * 50);
+                dynamic root = abilityAsset.RootObject;
+                ids.Add(root.Identifier);
+                paths.Add(abilityEntry.Name);
+                index++;
+            });
+            using (NativeWriter writer = new NativeWriter(new FileStream(CacheFilePath, FileMode.Create)))
+            {
+                writer.Write(version);
+                writer.Write(ids.Count);
+                for (int i = 0; i < ids.Count; i++)
+                {
+                    writer.Write(ids[i]);
+                    writer.WriteNullTerminatedString(paths[i]);
+                }
+            }
+        }
+    }
+
+    public class ViewAbilityIdLookupWindow : MenuExtension
+    {
+        //public override ImageSource Icon => new ImageSourceConverter().ConvertFromString("pack://application:,,,/BlueprintEditorPlugin;component/Images/HashingUtilsIcon.png") as ImageSource;
+
+        public override string TopLevelMenuName => "View";
+        public override string MenuItemName => "Ability Id Lookup";
+
+        public override RelayCommand MenuItemClicked => new RelayCommand(o =>
+        {
+            AbilityIdLookupWindow window = new AbilityIdLookupWindow();
+            if (!window.failed)
+                window.Show();
+        });
+    }
+}
---
+++ Windows/AbilityIdLookup.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using Frosty.Controls;
+using Frosty.Core;
+using Frosty.Core.Windows;
+using FrostySdk;
+using FrostySdk.IO;
+
+namespace AbilityIdLookupPlugin.Windows
+{
+    public partial class AbilityIdLookupWindow : FrostyDockableWindow
+    {
+        private Dictionary<uint, string> idToNameMap = new Dictionary<uint, string>();
+        public bool failed = false;
+
+        public AbilityIdLookupWindow()
+        {
+            FrostyTaskWindow.Show("Loading Ability Id Cache", "Loading Ability Id Cache...", (task) =>
+            {
+                task.Update("Loading Cache", 0);
+                if (LoadCache(task))
+                {
+                    task.Update("Cache Loaded", 100);
+                }
+                else
+                {
+                    task.Update("Cache Load Failed", 100);
+                    failed = true;
+                }
+            });
+            InitializeComponent();
+            if (failed)
+            {
+                Close();
+            }
+        }
+
+        private void IdInput_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                AbilityPath.Text = idToNameMap.TryGetValue(Convert.ToUInt32(IdInput.Text, CultureInfo.InvariantCulture), out string name) ? name : "Not Found (Added asset or modified id?)";
+            }
+            catch (FormatException)
+            {
+                AbilityPath.Text = "Invalid Id (make sure you only put in numbers)";
+            }
+        }
+
+        private void OkButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private bool LoadCache(FrostyTaskWindow task)
+        {
+            string cacheFilePath = AbilityIdGenerateCacheMenuExtension.CacheFilePath;
+            if (!File.Exists(cacheFilePath))
+            {
+                FrostyMessageBox.Show("Cache file not found. Please generate the cache first through \"Tools>Generate Cache>Ability Id Cache\".");
+                return false;
+            }
+            using (NativeReader reader = new NativeReader(new FileStream(cacheFilePath, FileMode.Open)))
+            {
+                int version = reader.ReadInt();
+                if (version != AbilityIdGenerateCacheMenuExtension.version)
+                {
+                    FrostyMessageBox.Show($"Cache file version mismatch. Please generate the cache again. (file: {version}, expected: {AbilityIdGenerateCacheMenuExtension.version})");
+                    return false;
+                }
+                int count = reader.ReadInt();
+                for (int i = 0; i < count; i++)
+                {
+                    uint id = reader.ReadUInt();
+                    string name = reader.ReadNullTerminatedString();
+                    idToNameMap[id] = name;
+                }
+            }
+            return true;
+        }
+    }
+}
